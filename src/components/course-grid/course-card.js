fa66c4f95a075bc08617571a12e89861a8bf64ed
anjun77 <anjunshao93@gmail.com>
2021-02-22 11:43:08 -0800

    Update course card

--- src/components/course-grid/course-card.js
+++ src/components/course-grid/course-card.js
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
 
-const CourseCard = ({course, deleteCourse, updateCourse}) =>
-    <div className="col-4">
-      <div className="card">
-        <img
-            src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
-            className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">{course.title}</h5>
-          <p className="card-text">Some quick example text to build on the card
-            title and make up the bulk of the card's
-            content.</p>
-          <img src={``}/>
-          <Link to="/courses/editor" className="btn btn-primary">
-            {course.title}
-          </Link>
-          <i className="fas fa-trash"></i>
+const CourseCard = ({course, deleteCourse, updateCourse}) => {
+  const [editing, setEditing] = useState(false)
+  const [title, setTitle] = useState(course.title)
+
+  const saveTitle = () => {
+    setEditing(false)
+    const newCourse = {
+      ...course,
+      title: title
+    }
+    updateCourse(newCourse)
+  }
+
+  return (
+      <div className="col-4">
+        <div className="card">
+          {editing && <i onClick={() => saveTitle()}
+                         className="fas fa-check"></i>}
+          {editing && <i onClick={() => deleteCourse(course)}
+                         className="fas fa-trash"></i>}
+          <img
+              src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
+              className="card-img-top" alt="..."/>
+
+          <div className="card-body">
+            {!editing && <h5 className="card-title">{course.title}</h5>}
+            {editing && <input type="text" onChange={(event) => setTitle(
+                event.target.value)}
+                               value={title}/>}
+            <p className="card-text">Some quick example text to build on the
+              card
+              title and make up the bulk of the card's
+              content.</p>
+            <img src={``}/>
+            <Link to="/courses/editor" className="btn btn-primary">
+              {course.title}
+            </Link>
+            <div>
+              {!editing && <i onClick={() => {
+                setEditing(true);
+                setTitle(course.title)
+              }} className='fas fa-edit'></i>}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-
+  )
+}
 
 export default CourseCard
--- src/components/course-grid/course-grid.js
+++ src/components/course-grid/course-grid.js
@@ -3,28 +3,37 @@ import CourseCard from "./course-card";
 import {Link} from "react-router-dom";
 import {deleteCourse} from "../../services/course-service";
 
-const CourseGrid = ({courses}) =>
+const CourseGrid = ({courses, deleteCourse, updateCourse}) =>
     <div>
+      <table className="table">
+        <thead>
+        <tr>
+          <th>
+            Recent Documents
+          </th>
+          <th>
+            Owned by me
+          </th>
+          <th>
+            <i className="fas fa-folder"></i>
+            <i className="fas fa-sort-alpha-down"></i>
+            <Link to="/courses/table">
+              <i className="fas fa-th"></i>
+            </Link>
+          </th>
+        </tr>
+        </thead>
+      </table>
+
       <div className="row">
-        <div className="col-4">Recent Documents</div>
-        <div className="col-4">Owned by me</div>
-        <div>
-          <i className="fas fa-folder"></i>
-          <i className="fas fa-sort-alpha-down"></i>
-        </div>
-        <Link to="/courses/table">
-          <i className="fas fa-th"></i>
-        </Link>
-        <div className="row">
-          {
-            courses.map((course, ndx) =>
-                <CourseCard key={ndx}
-                            course={course}
-                            deleteCourse={deleteCourse}/>
-                            // updateCourse={updateCourse}/>
-            )
-          }
-        </div>
+        {
+          courses.map((course, ndx) =>
+                  <CourseCard key={ndx}
+                              course={course}
+                              updateCourse={updateCourse}
+                              deleteCourse={deleteCourse}/>
+          )
+        }
       </div>
     </div>
 
