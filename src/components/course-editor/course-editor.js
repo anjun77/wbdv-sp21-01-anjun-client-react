f77f33e28621c3666c4d493d97257802a806ace2
anjun77 <anjunshao93@gmail.com>
2021-02-21 20:59:47 -0800

    Modified App.js

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
-const CourseEditor = () => {
-  return (
-      <h1>Course Editor</h1>
-  )
-}
+// const CourseEditor = ({props}) =>
+const CourseEditor = ({history}) =>
+    <div>
+      <h2>
+        Course Editor
+        <i onClick={() => history.goBack()}
+           className="fas fa-times float-right"></i>
+        {/*<i onClick={() => props.history.goBack()}*/}
+        {/*   className="fas fa-times float-right"></i>*/}
+      </h2>
+    </div>
 
 export default CourseEditor
