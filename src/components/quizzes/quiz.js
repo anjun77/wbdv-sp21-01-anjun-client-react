a1150a9d5bc1170273ce77bd7552e939ea3b8ca5
anjun77 <anjunshao93@gmail.com>
2021-04-20 14:36:46 -0700

    quiz

--- src/components/quizzes/quiz.js
+++ src/components/quizzes/quiz.js
@@ -1,92 +1,80 @@
 import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {useParams} from "react-router-dom"
 import Question from "./questions/question";
-import QuestionService from "../../services/question-service"
-import QuizService from "../../services/quizzes-service"
-import AttemptService from "../../services/attemp-service"
+import questionService from "../../services/question-service"
+import quizService from "../../services/quiz-service"
+import attemptService from "../../services/attempt-service"
 
 const Quiz = () => {
-  const {quizId} = useParams();
-  const [questions, setQuestions] = useState([]);
-  const [quiz, setQuiz] = useState([]);
-  const [currentScore, setCurrentScore] = useState(0);
-  const [questionsWithAns, setQuestionsWithAns] = useState([]);
-  const [isGraded, setIsGraded] = useState(false);
-  const [attempts, setAttempts] = useState([]);
-  const [attemptHistory, setAttemptHistory] = useState(false);
-
-  useEffect(() => {
-    QuestionService.findQuestionsForQuiz(quizId).then((questions) => {
-      setQuestions(questions)
-      setQuestionsWithAns(questions)
+  const {quizId} = useParams()
+  const [questions, setQuestions] = useState([])
+  const [quiz, setQuiz] = useState([])
+  const [gradeStatus,beginGrade] = useState(false)
+  const [attempts, setAttempts] = useState([])
+  const [currentAttempt, setCurrentAttempt] = useState([])
+  const findAttempt = () => {
+    attemptService.findAttemptsForQuiz(quizId)
+    .then((attempts) => {
+      setAttempts(attempts)
     })
-    QuizService.findQuizById(quizId).then((quiz) => setQuiz(quiz))
-
-  }, [quizId])
-
+  }
   useEffect(() => {
-    QuizService.findAttemptsByQuizId(quizId).then((attempts) => setAttempts(attempts))
-  }, [quizId])
+    quizService.findQuizById(quizId)
+    .then((quiz) => {
+      setQuiz(quiz)
+    })
+    questionService.findQuestionsForQuiz(quizId)
+    .then((questions) => {
+      setQuestions(questions)
+    })
 
-  return (
-      <div className="container">
-        <h3>{quiz.title} ({questions.length})</h3>
-        <ul className='list-group'>
+  }, [])
+  return(
+      <div>
+        <h3>{quiz.title} </h3>
+        <ul className={"list-group"}>
           {
             questions.map((question) => {
-              return (
-                  <li>
-                    <Question
-                        question={question}
-                        isGraded={isGraded}
-                        setQuestionsWithAns={setQuestionsWithAns}
+              return(
+                  <li className={"list-group-item my-3 border-top"}>
+                    <Question question={question}
+                              gradeStatus = {gradeStatus}
                     />
                   </li>
               )
             })
           }
-          <li className="list-group-item">
-            {
-              isGraded &&
-              <span>Score: {currentScore}</span>
-            }
-            <button
-                onClick={()=> {
-                  setIsGraded(true)
-                  if (!isGraded) {
-                    QuizService.submitQuiz(quizId, questionsWithAns)
-                    .then(attempt => setCurrentScore(attempt.score))
-                  }
-                }}
-                className="btn btn-success float-right"
-                type = 'button'
-            >
-              Submit
-            </button>
-          </li>
-          <li className="list-group-item">
-            <button
-                onClick={()=> {
-                  setAttemptHistory(!attemptHistory)
-                }}
-                className="btn btn-primary float-right"
-                type = 'button'
-            >
-              Histories
-            </button>
-          </li>
-
-          {
-            attemptHistory &&
-            attempts.map((attempt) => {
-              return (
-                  <li className="list-group-item">
-                    Last Score: {attempt.score}
-                  </li>
-              )
-            })
-          }
         </ul>
+        <div type="button" className="btn btn-success my-3" onClick={() => {
+          beginGrade(true)
+          quizService.submitQuiz(quiz._id,questions).then((attempt)=> {
+            findAttempt();
+            setCurrentAttempt(attempt)
+          })
+        }}>Submit</div>
+
+        {
+          gradeStatus &&
+
+          <ul className={"list-group my-2"}>
+            <h6>Current Score : {currentAttempt.score}</h6>
+
+            {
+              attempts.length > 1 &&
+              <h6>Past Attempt:</h6>
+            }
+            {
+              attempts.filter((attempt) => attempt._id !== currentAttempt._id).map((attempt) => {
+                return (
+                    <li className={"list-group-item"}>
+                      <h6>Attempt Time : {attempt.createdAt}</h6>
+                      <h6>Score : {attempt.score}</h6>
+                    </li>
+                )
+              })
+            }
+          </ul>
+        }
       </div>
   )
 }
